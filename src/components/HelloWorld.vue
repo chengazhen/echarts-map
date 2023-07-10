bc33a9d8a4f756031994c318c4a8e67bb3f31360
cheng chong zhen <52627267+chengchongzhen@users.noreply.github.com>
2023-07-10 09:44:59 +0000

    Update 4 files

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,49 +1,63 @@
 <template>
-  <div style="width: 100%; height: 100%;">
+  <div style="width: 100%; height: 100%">
     <v-chart class="chart" :option="option" ref="map" @click="handleClick" />
   </div>
 </template>
 
 <script setup>
-import { provinces } from './mapcode'
+import { provinces } from "./mapcode";
 import { onMounted } from "vue";
-import { getMapJson, getOption } from "./index";
-import getLoadMap from './loadMap'
-import { use, registerMap, } from "echarts/core";
+import getLoadMap from "./loadMap";
+import { use, registerMap } from "echarts/core";
 import { CanvasRenderer } from "echarts/renderers";
-import { PieChart, MapChart, ScatterChart, EffectScatterChart  } from "echarts/charts";
+import {
+  PieChart,
+  MapChart,
+  ScatterChart,
+  EffectScatterChart,
+} from "echarts/charts";
 import {
   TitleComponent,
   TooltipComponent,
   LegendComponent,
   VisualMapComponent,
-  GridComponent
+  GridComponent,
 } from "echarts/components";
 import VChart, { THEME_KEY } from "vue-echarts";
 import { ref, provide } from "vue";
 
+const map = ref();
 
-const map = ref()
-
-let loadMap = null
+let loadMap = null;
 
 const getUrl = (code) => {
-  return `https://geo.datav.aliyun.com/areas_v3/bound/${code}_full.json`
-}
+  return `../mapData/${code}_full.json`;
+};
+
+// onMounted(() => {
+//   loadMap = getLoadMap(map.value, registerMap);
+//   loadMap.init(getUrl(100000), "河南省", []);
+// });
+
+// const handleClick = (params) => {
+//   const code = provinces[params.name];
+//   if (code) {
+//     loadMap.init(getUrl(code), params.name, []);
+//   }
+// };
 
 onMounted(() => {
-  loadMap = getLoadMap(map.value, registerMap)
-  loadMap.init(getUrl(410000), '河南省', [])
-})
+  loadMap = getLoadMap(map.value, registerMap);
+  loadMap.initByCode(100000, "河南省", []);
+});
 
 const handleClick = (params) => {
-  const code = provinces[params.name]
+  const code = provinces[params.name];
+  console.log(code);
   if (code) {
-    loadMap.init(getUrl(code), params.name, [])
+    loadMap.initByCode(410000, params.name, []);
   }
-}
-
-
+};
 
 use([
   CanvasRenderer,
@@ -55,7 +69,7 @@ use([
   VisualMapComponent,
   GridComponent,
   ScatterChart,
-  EffectScatterChart 
+  EffectScatterChart,
 ]);
 
 provide(THEME_KEY, "dark");
@@ -63,7 +77,7 @@ provide(THEME_KEY, "dark");
 const option = ref();
 </script>
 
-<style >
+<style>
 html,
 body,
 #app {
